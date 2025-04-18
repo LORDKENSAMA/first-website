ff8c16e35583a3817cc25e5ec13f4f2bfc318dbb
LORDKENSAMA <billyhvan@icloud.com>
2025-04-18 18:18:45 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyCUvPZM7a7ciEvAMB1kDudc6ROnoWPGqvg",
+  authDomain: "kensamawebsite.firebaseapp.com",
+  projectId: "kensamawebsite",
+  storageBucket: "kensamawebsite.appspot.com",
+  messagingSenderId: "33823463857",
+  appId: "1:33823463857:web:477a5078ce99cdd5f78590",
+  measurementId: "G-CVL61HZMY7"
+};
+
+firebase.initializeApp(firebaseConfig);
